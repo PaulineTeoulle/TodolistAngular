b2da79b47424b78b4e540eff0954aa303cf163c1
Pauline <pa.teoulle@gmail.com>
2021-12-18 16:54:03 +0100

    Extract method from service : clean code

--- src/app/speechRecognition.service.ts
+++ src/app/speechRecognition.service.ts
@@ -15,38 +15,57 @@ export class SpeechRecognitionService {
 
   record(): Observable<string> {
     return Observable.create((observer:any) => {
-      const { webkitSpeechRecognition }: IWindow = <IWindow>(<any>window);
-      this.speechRecognition = new webkitSpeechRecognition();
-      this.speechRecognition.continuous = false;
-      this.speechRecognition.interimresults = true;
-      this.speechRecognition.lang = "fr";
-
-      this.speechRecognition.onresult = (speech: any) => {
-        let text: string = "";
-        if (speech.results) {
-          var result = speech.results[speech.resultIndex];
-          var transcript = result[0].transcript;
-            if (result.isFinal) {
-              if (result[0].confidence < 0.3) {
-                console.log("Pas de reconnaissance de texte -- Veuillez réessayer");
-              } else {
-                text = _.trim(transcript);
-              }
-            }
-        }
-        this.zone.run(() => {
-          observer.next(text);
-        });
-      };
-
-      this.speechRecognition.onerror = function (error : any) {
-        observer.error(error);
-      };
-      this.speechRecognition.onend = () => {
-        observer.complete();
-      };
-      this.speechRecognition.start();
+      this.setupService();
+      this.getStringResult(observer);
+      this.handleError(observer);
+      this.handleEnd(observer);
+      this.handleStart();
     });
+  }
+
+  setupService(){
+    const { webkitSpeechRecognition }: IWindow = <IWindow>(<any>window);
+    this.speechRecognition = new webkitSpeechRecognition();
+    this.speechRecognition.continuous = false;
+    this.speechRecognition.interimresults = true;
+    this.speechRecognition.lang = "fr";
+  }
+
+  getStringResult(observer:any){
+    this.speechRecognition.onresult = (speech: any) => {
+      let text: string = "";
+      if (speech.results) {
+        var result = speech.results[speech.resultIndex];
+        var transcript = result[0].transcript;
+          if (result.isFinal) {
+            if (result[0].confidence < 0.3) {
+              console.log("Pas de reconnaissance de texte -- Veuillez réessayer");
+            } else {
+              text = _.trim(transcript);
+              console.log("Texte entendu : " + text);
+            }
+          }
+      }
+      this.zone.run(() => {
+        observer.next(text);
+      });
+    };
+  }
+
+  handleError(observer:any){
+    this.speechRecognition.onerror = function (error : any) {
+      observer.error(error);
+    };
+  }
+
+  handleStart(){
+    this.speechRecognition.start();
+  }
+
+  handleEnd(observer:any){
+    this.speechRecognition.onend = () => {
+      observer.complete();
+    };
   }
 }
 
